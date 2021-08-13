89db84afb7c461a76da5cef76d8fe94ce16db7e3
sarhakor <74352839+sarhakor@users.noreply.github.com>
2021-08-13 17:34:20 +0900

    Update videocoding.sh

--- pakages/videocoding.sh
+++ pakages/videocoding.sh
@@ -1,10 +1,13 @@
 # install cmake and parallel
 USER=$(whoami)
 
+mkdir /home/${USER}/lib
+
 # install cmake dependency (openssl)
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install build-essential
 sudo apt install wget
+cd /home/${USER}/lib
 wget http://www.openssl.org/source/openssl-1.1.1d.tar.gz
 tar zxf openssl-1.1.1d.tar.gz
 cd openssl-1.1.1d
@@ -15,7 +18,6 @@ sudo make install
 
 # install cmake
 cmakever=cmake-3.16.2
-mkdir /home/${USER}/lib
 cd /home/${USER}/lib
 wget https://cmake.org/files/v3.16/${cmakever}.tar.gz
 tar -xvzf ${cmakever}.tar.gz
